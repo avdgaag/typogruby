10e79d7b5a4671493ce579d265859d97caa65010
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2014-05-25 12:25:20 +0100

    Fix regex pattern for finding encoded ampersands

--- lib/typogruby.rb
+++ lib/typogruby.rb
@@ -160,10 +160,10 @@ module Typogruby
     exclude_sensitive_tags(text) do |t|
       # $1 and $2 are excluded HTML tags, $3 is the part before the caps and $4 is the caps match
       t.gsub(%r{
-          (<[^/][^>]*?>)|                                     # Ignore any opening tag, so we don't mess up attribute values
-          (\s|&nbsp;|^|'|"|>|)                                # Make sure our capture is preceded by whitespace or quotes
-          ([A-Z\d](?:[\.'&]?(?:amp;|#38;)?[A-Z\d][\.']?){1,}) # Capture capital words, with optional dots, numbers or ampersands (&, &amp; or &#38;) in between
-          (?!\w)                                              # ...which must not be followed by a word character.
+          (<[^/][^>]*?>)|                                      # Ignore any opening tag, so we don't mess up attribute values
+          (\s|&nbsp;|^|'|"|>|)                                 # Make sure our capture is preceded by whitespace or quotes
+          ([A-Z\d](?:[\.'&]?(&amp;|&\#38;)?[A-Z\d][\.']?){1,}) # Capture capital words, with optional dots, numbers or ampersands in between
+          (?!\w)                                               # ...which must not be followed by a word character.
         }x) do |str|
         tag, before, caps = $1, $2, $3
 
